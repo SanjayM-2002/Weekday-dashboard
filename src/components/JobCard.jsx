3e63625c079aec8dfe0258ebf32ada4812f3396c
SanjayM-2002 <sanjaymshivakripa@gmail.com>
2024-05-01 23:26:26 +0530

    job card done

--- src/components/JobCard.jsx
+++ src/components/JobCard.jsx
@@ -1,22 +1,32 @@
-import { Box, Paper, Stack } from '@mui/material';
+import {
+  Avatar,
+  AvatarGroup,
+  Box,
+  Button,
+  Paper,
+  Stack,
+  Typography,
+} from '@mui/material';
 import React, { useState } from 'react';
+import BoltIcon from '@mui/icons-material/Bolt';
 
-const JobCard = () => {
+const JobCard = ({ job }) => {
   const [hovered, setHovered] = useState(false);
-  const [jobData, setJobData] = useState({
-    jdLink: 'https://weekday.works',
-    jdUid: 'cfff35ac-053c-11ef-83d3-06301d0a7178-92010',
-    jobDetailsFromCompany:
-      "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
-    jobRole: 'frontend',
-    location: 'delhi ncr',
-    maxExp: null,
-    maxJdSalary: null,
-    minExp: null,
-    minJdSalary: null,
-    salaryCurrencyCode: null,
-  });
+  //   const [jobData, setJobData] = useState({
+  //     jdLink: 'https://weekday.works',
+  //     jdUid: 'cfff35ac-053c-11ef-83d3-06301d0a7178-92010',
+  //     jobDetailsFromCompany:
+  //       "This is a sample job and you must have displayed it to understand that its not just some random lorem ipsum text but something which was manually written. Oh well, if random text is what you were looking for then here it is: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages and now in this assignment.",
+  //     jobRole: 'frontend',
+  //     location: 'delhi ncr',
+  //     maxExp: null,
+  //     maxJdSalary: null,
+  //     minExp: null,
+  //     minJdSalary: null,
+  //     salaryCurrencyCode: null,
+  //   });
   const [mockData, setMockData] = useState({
+    companyName: 'Firefly',
     estimatedSalary: '10-15 LPA',
   });
   return (
@@ -24,6 +34,7 @@ const JobCard = () => {
       elevation={hovered ? 4 : 1}
       sx={{
         backgroundColor: 'white',
+        margin: 2,
         padding: 4,
         borderRadius: 4,
         transition: 'box-shadow 0.3s',
@@ -72,14 +83,48 @@ const JobCard = () => {
               src='https://images.unsplash.com/photo-1512917774080-9991f1c4c750?auto=format&w=350&dpr=2'
             />
             <Stack>
-              <Box>Firefly</Box>
-              <Box>{jobData.jobRole}</Box>
-              <Box>{jobData.location}</Box>
+              <Typography variant='body1'>{mockData.companyName}</Typography>
+              <Typography variant='body1'>{job.jobRole}</Typography>
+              <Typography variant='body1'>{job.location}</Typography>
             </Stack>
           </Stack>
-          <Stack direction={'row'}>
-            <Box>Estimated Salary: </Box>
-            <Box>&#8377;{mockData.estimatedSalary} &#x2713;</Box>
+          <Stack direction={'row'} spacing={1}>
+            <Typography variant='body1'>Estimated Salary: </Typography>
+            <Typography variant='body1'>
+              &#8377;{mockData.estimatedSalary} &#x2713;
+            </Typography>
+          </Stack>
+          <Typography variant='h5'>About Company:</Typography>
+          <Typography variant='h6'>About us</Typography>
+          <Typography variant='body2'>{job.jobDetailsFromCompany}</Typography>
+          <Button variant='text'>View Job</Button>
+          <Typography variant='h6'>Minimum Experience: </Typography>
+          <Typography variant='body1'>
+            {job.minExp > 0 ? job.minExp : 0} Years
+          </Typography>
+          <Stack spacing={1}>
+            <Button variant='contained' startIcon={<BoltIcon />}>
+              Easy Apply
+            </Button>
+            <Button
+              variant='contained'
+              startIcon={
+                <AvatarGroup>
+                  <Avatar
+                    alt='John doe'
+                    src='avatar1.jpg'
+                    sx={{ width: 24, height: 24 }}
+                  />
+                  <Avatar
+                    alt='Remy Sharp'
+                    src='avatar2.jpg'
+                    sx={{ width: 24, height: 24 }}
+                  />
+                </AvatarGroup>
+              }
+            >
+              Unlock referal asks
+            </Button>
           </Stack>
         </Stack>
       </Box>
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,46 +1,86 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import JobCard from '../components/JobCard';
-import { Grid } from '@mui/material';
+import { Box, CircularProgress, Grid } from '@mui/material';
+import { Oval } from 'react-loader-spinner';
 
 const Dashboard = () => {
+  const [jobData, setJobData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
   const fetchData = async () => {
     const myHeaders = new Headers();
     myHeaders.append('Content-Type', 'application/json');
-
     const raw = JSON.stringify({
-      limit: 8,
-      offset: 0,
+      limit: 10,
+      offset: page * 10,
     });
-
     const requestOptions = {
       method: 'POST',
       headers: myHeaders,
       body: raw,
     };
 
-    const res = await fetch(
-      'https://api.weekday.technology/adhoc/getSampleJdJSON',
-      requestOptions
-    );
-    const data = await res.json();
-    console.log('data is: ', data);
-    console.log('1st job data is: ', data.jdList[1]);
+    try {
+      const res = await fetch(
+        'https://api.weekday.technology/adhoc/getSampleJdJSON',
+        requestOptions
+      );
+      const data = await res.json();
+      console.log('data is: ', data);
+      console.log('1st job data is: ', data.jdList[0]);
+      console.log('keys are: ', Object.keys(data.jdList[0]));
+      setJobData((prevData) => [...prevData, ...data.jdList]);
+      console.log('jobdata is: ', jobData);
+      setPage(page + 1);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setIsLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+    console.log('page fetching is: ', page);
+  }, []);
+  const handleScroll = () => {
+    if (
+      window.innerHeight + window.scrollY >=
+        document.documentElement.offsetHeight ||
+      isLoading
+    ) {
+      fetchData();
+      console.log('loading is : ', isLoading);
+    }
   };
 
-  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [page]);
 
   return (
     <>
-      <div>
-        <JobCard />
-      </div>
-      {/* <Grid container my={4}>
-        {arr.map((i) => (
-          <Grid item key={i} xs={12} sm={12} md={6} lg={4}>
-            <JobCard />
-          </Grid>
-        ))}
-      </Grid> */}
+      {/* <div><JobCard /></div> */}
+      <Grid container my={4}>
+        {jobData.length &&
+          jobData.map((job, index) => (
+            <Grid item key={index} xs={12} sm={12} md={6} lg={4}>
+              <JobCard job={job} />
+            </Grid>
+          ))}
+      </Grid>
+
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Oval
+          visible={true}
+          height='80'
+          width='80'
+          color='#4fa94d'
+          ariaLabel='oval-loading'
+          wrapperStyle={{}}
+          wrapperClass=''
+        />
+      </Box>
     </>
   );
 };
